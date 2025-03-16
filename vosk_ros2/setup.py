e383ffcc881b6ec1d9912f67c1d8cca69563c593
Maik Knof <knofm@hs-weingarten.de>
2025-03-16 20:48:48 +0100

    setup ros package

---
+++ vosk_ros2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os 
+from glob import glob
+
+package_name = 'vosk_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Maik Knof',
+    maintainer_email='Maik.Knof@gmx.de',
+    description='ROS2 wrapper for Vosk',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vosk_ros2_node = vosk_ros2.vosk_ros2_node:main'
+        ],
+    },
+)
