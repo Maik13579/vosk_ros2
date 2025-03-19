748274db793b5de7fcc9e2f25b4a6df7be468669
Maik Knof <knofm@hs-weingarten.de>
2025-03-19 11:20:47 +0100

    split installing deps from build to enable downloading models first

--- docker/build.sh
+++ docker/build.sh
@@ -16,22 +16,28 @@ docker build -t ${IMAGE_TAG} --target base \
   -f $PARENT_DIR/docker/Dockerfile \
   $PARENT_DIR
 
+# Install deps
+docker build -t ${IMAGE_TAG} --target deps \
+  --build-arg BASE_IMAGE=ros:${ROS_DISTRO} \
+  -f $PARENT_DIR/docker/Dockerfile \
+  $PARENT_DIR
+
 if $DOWNLOAD_MODELS; then
-  docker build -t ${IMAGE_TAG} --target download_models\
+  docker build -t ${IMAGE_TAG} --target download_models \
     --build-arg BASE_IMAGE=${IMAGE_TAG} \
     -f $PARENT_DIR/docker/Dockerfile \
     $PARENT_DIR
 fi
 
 if $BUILD_CUSTOM_MODELS; then
-  docker build -t ${IMAGE_TAG} --target build_custom_models\
+  docker build -t ${IMAGE_TAG} --target build_custom_models \
     --build-arg BASE_IMAGE=${IMAGE_TAG} \
     -f $PARENT_DIR/docker/Dockerfile \
     $PARENT_DIR
 fi
 
 #Build vosk and ros wrapper
-docker build -t ${IMAGE_TAG} --target build\
+docker build -t ${IMAGE_TAG} --target build \
   --build-arg BASE_IMAGE=${IMAGE_TAG} \
   -f $PARENT_DIR/docker/Dockerfile \
   $PARENT_DIR
